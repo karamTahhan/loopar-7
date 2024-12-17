b69175a162b946ba6f4de458450ad589dc7a66b5
karamTahhan <kata07001@utb.vaxjo.se>
2024-12-17 13:53:18 +0100

    Add project files.

---
+++ loopar 7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System; 
+
+namespace loopar_7
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int tal;
+            Console.WriteLine("skriv in ett positiv tal. Negativ tal när du vill avsluta");
+
+            while (true)
+            {
+                Console.WriteLine("skriv in ett tal");
+                tal=int.Parse(Console.ReadLine());
+                if (tal < 0 )
+                {
+                    Console.WriteLine("du skrev ett negativit tal programmet avslutas");
+                    break;
+
+                }
+
+                    
+
+
+            }
+
+        }
+
+    }
+
+
+}
